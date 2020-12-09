9c5f0f4229230f075c7b83e688e8a78b78afcd4b
Braden East <bradeneastdesign@gmail.com>
2020-12-09 16:07:50 -0600

    update root domain

--- api/updateUserData.js
+++ api/updateUserData.js
@@ -1,8 +1,14 @@
 import GoTrue from 'gotrue-js';
 
 // Instantiate the GoTrue auth client with an optional configuration
-export default new GoTrue({
+const auth = new GoTrue({
   APIUrl: 'https://flintstone.app/.netlify/identity',
   audience: '',
   setCookie: false,
 });
+
+exports.handler = async (event, context) => {
+  return await auth
+    .currentUser()
+    .update({ flintstone_data: JSON.stringify(event.body) });
+}
