cf390f56dcdc6a122a0dcafce39934c19c1b7266
Braden East <bradeneastdesign@gmail.com>
2020-12-11 16:52:02 -0600

    fix minor layout bugs

--- source/js/components/StylesPane.js
+++ source/js/components/StylesPane.js
@@ -1,12 +1,22 @@
-import { html } from "lit-html";
-import state from "../state";
+import { html, nothing } from "lit-html";
+import state, { setState } from "../state";
 import { tags } from "../style_data";
 import StyleAdjustment from "./StyleAdjustment";
 
 
 export default () => html`
 <div ?data-active=${state.showStyles} class="styles">
-  <h2>Styles</h2>
+  <div class=styles__header>
+    <h2>Styles</h2>
+    ${
+      state.showPreview
+      ? nothing
+      : html`
+      <span class=hint>
+        Hint: <button class="link primary" @click=${() => setState('showPreview', true)}>Show the preview</button> to see your changes.
+      </span>`
+    }
+  </div>
   <div class=adjustments>
     ${Object.entries(tags).map(StyleAdjustment)}
   </div>
