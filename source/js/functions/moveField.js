e5ed7f2a04afd314de7273b2d73f8043f5e7df84
Braden East <bradeneastdesign@gmail.com>
2020-12-14 08:39:40 -0600

    add field move animations

--- source/js/functions/moveField.js
+++ source/js/functions/moveField.js
@@ -3,12 +3,35 @@ import { $ } from "../utils";
 
 export default (fieldIndex, amount) => {
 
+  document.activeElement.blur();
+
+  let selectFieldByIndex = index => $(`.fields li[data-index="${index}"]`);
+  let setCustomProp = (elem, prop, value) => elem.style.setProperty(`--${prop}`, value);
   let direction = amount > 0 ? 'down' : 'up';
   let field = state.currentDataset.fields[fieldIndex];
   let newIndex = fieldIndex + amount;
+
   state.currentDataset.fields.splice(fieldIndex, 1);
   state.currentDataset.fields.splice(newIndex, 0, field);
-
   setState('currentDataset', state.currentDataset, true);
-  $(`.fields [data-index="${newIndex}"] [title*="${direction}"]`).focus();
+
+  let currentField = selectFieldByIndex(fieldIndex);
+  let targetField = selectFieldByIndex(newIndex);
+
+  setCustomProp(currentField, 'amt', amount);
+  setCustomProp(targetField, 'amt', amount * -1);
+
+  for (let { style } of [currentField, targetField]) {
+    style.animation = 'none';
+    style.transition = 'none';
+  }
+
+  setTimeout(() => {
+    for (let li of [currentField, targetField]) {
+      li.style.transition = '';
+      setCustomProp(li, 'amt', 0);
+    }
+    $(`[title*="${direction}"]`, targetField).focus();
+  }, 10);
+
 }
