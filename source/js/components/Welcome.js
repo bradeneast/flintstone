04a159d0294e9325f7de9a38f38835ab3b67ce3e
Braden East <bradeneastdesign@gmail.com>
2021-01-08 15:28:31 -0600

    add wrapper to welcome content

--- source/js/components/Welcome.js
+++ source/js/components/Welcome.js
@@ -7,42 +7,46 @@ import Modal from "./Modal";
 
 
 export default () => html`
-<div>
-  <h1 class=mount>Welcome to <span style="color: var(--primary)">Flintstone</span>!</h1>
-  <p>The tool for creating formal-ish documents on the go.</p>
-</div>
-<form class="mount">
-${
-  Button({
-    content: 'Start from scratch',
-    className: 'primary',
-    action: () => defaultState
-      .then(defaultState => {
-        state.savedLocally = true;
-        state.currentUser.styles = defaultState.currentUser.styles;
-        prepState();
-        renderAll();
-        autoSave(true);
-        console.log(state.currentUser.styles);
+<div style="display: grid; place-items: center; gap: 2em;">
+
+  <div>
+    <h1 class=mount>Welcome to <span style="color: var(--primary)">Flintstone</span>!</h1>
+    <p>The tool for creating formal-ish documents on the go.</p>
+  </div>
+
+  <form class="mount">
+  ${
+    Button({
+      content: 'Start from scratch',
+      className: 'primary',
+      action: () => defaultState
+        .then(defaultState => {
+          state.savedLocally = true;
+          state.currentUser.styles = defaultState.currentUser.styles;
+          prepState();
+          renderAll();
+          autoSave(true);
+          console.log(state.currentUser.styles);
+        })
     })
-  })
-}
-${
-  Button({
-    content: 'Use the starter template',
-    className: 'link',
-    action: () => defaultState
-      .then(defaultState => {
-        serialize(defaultState, state);
-        state.savedLocally = true;
-        prepState();
-        renderAll();
-        autoSave(true);
-      })
-      .catch(err => {
-        handleError(err);
-        renderAll(Modal(AuthError(err.message)));
-      })
-  })
-}
-</form>`;
+    }
+  ${Button({
+      content: 'Use the starter template',
+      className: 'link',
+      action: () => defaultState
+        .then(defaultState => {
+          serialize(defaultState, state);
+          state.savedLocally = true;
+          prepState();
+          renderAll();
+          autoSave(true);
+        })
+        .catch(err => {
+          handleError(err);
+          renderAll(Modal(AuthError(err.message)));
+        })
+    })
+  }
+  </form>
+  
+</div>`;
