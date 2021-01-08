f53fd9f39e18a416690d66aecded3cf5b966a96f
Braden East <bradeneastdesign@gmail.com>
2021-01-08 15:31:20 -0600

    remove close button

--- source/js/components/Styles/StylesPane.js
+++ source/js/components/Styles/StylesPane.js
@@ -1,9 +1,6 @@
 import { html } from "lit-html";
-import togglePane from "../../functions/togglePane";
 import state, { setState } from "../../state";
 import { tags } from "../../style_data";
-import Button from "../Button";
-import Icons from "../Icons";
 import StyleAdjustment from "./StyleAdjustment";
 
 
@@ -12,28 +9,13 @@ export default () => html`
   <div class=styles__header>
   ${
     state.showPreview
-      ? html`
-      <p class=light>
-        Change the look and feel of your document here.
-      </p>`
+      ? html`<p class=light>Change the look and feel of your document here.</p>`
       : html`
       <p class=light>
         <button class="link primary" @click=${()=> setState('showPreview', true)}>Show the document preview</button> to see your
         changes.
       </p>`
   }
-  <actions>
-  ${
-    Button({
-      title: 'Close style editor',
-      className: 'icon',
-      icon: html`<mobile-only>${Icons.down}</mobile-only><desktop-only>${Icons.right}</desktop-only>`,
-      action: () => togglePane('styles', false)
-    })
-  }
-  </actions>
   </div>
-  <div class=adjustments>
-    ${Object.entries(tags).map(StyleAdjustment)}
-  </div>
+  <div class=adjustments>${Object.entries(tags).map(StyleAdjustment)}</div>
 </div>`;
