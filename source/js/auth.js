093cbce0db0b84f3c86f1d1ac00790b848b40751
Braden East <bradeneastdesign@gmail.com>
2022-11-27 14:35:54 -0600

    change url from custom domain to netlify-provided domain

--- source/js/auth.js
+++ source/js/auth.js
@@ -2,7 +2,7 @@ import GoTrue from 'gotrue-js';
 
 // Instantiate the GoTrue auth client with an optional configuration
 export default new GoTrue({
-  APIUrl: 'https://flintstone.app/.netlify/identity',
+  APIUrl: 'https://flintstone-app.netlify.app/.netlify/identity',
   audience: '',
   setCookie: true,
 });
